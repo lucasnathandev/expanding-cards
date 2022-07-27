d89b3bdbe64527cdece3d3808f2d5bbaa56cb1c6
Solium Dev <lucas.develop.prog@gmail.com>
2022-07-27 14:04:52 -0300

    :sparkles: Expanding cards project completed.

---
+++ script.js
@@ -0,0 +1,38 @@
+const panels = document.querySelectorAll(".panel")
+
+// Functions definition area
+
+function resetActive() {
+  panels.forEach((element) => {
+    element.classList.remove("active")
+  })
+}
+
+function setActive(element) {
+  element.classList.toggle("active")
+}
+
+function findActiveIndex() {
+  const index = Array.from(panels).findIndex((element) =>
+    element.classList.contains("active")
+  )
+  return index
+}
+
+function main() {
+  const activePanelIndex = findActiveIndex()
+  if (activePanelIndex > -1) {
+    resetActive()
+  }
+  panels.forEach((panel) =>
+    panel.addEventListener("pointerup", (e) => {
+      if (e.currentTarget.classList.contains("active")) {
+        return resetActive()
+      }
+      resetActive()
+      setActive(e.currentTarget)
+    })
+  )
+}
+// Code execution
+main()
